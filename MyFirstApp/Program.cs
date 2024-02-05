686ed8c3558c861367fe0ee167089e06df894c61
Trewor-Vincent <Trewor.Ploomipuu@tktk.ee>
2024-02-05 21:52:42 +0200

    make simple app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What your name?");
+
+string username = Console.ReadLine();
+
+Console.WriteLine($"Hello, {username}!");
+
